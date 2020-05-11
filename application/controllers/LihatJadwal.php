cb10a3dfe01aa70c20f65e430c0fe9f65bb9c1ec
rama.github <46712606+rmd1999@users.noreply.github.com>
2020-05-11 10:59:04 +0700

    LihatJadwa,Login API

---
+++ application/controllers/LihatJadwal.php
@@ -0,0 +1,43 @@
+<?php
+
+defined ('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class LihatJadwal extends REST_Controller{
+
+	function __construct($config = 'rest'){
+		parent::__construct($config);
+	}
+
+	function LihatJadwalDosen_get(){
+        // select * from tik.jadwal_kul where nip = '4617010043')
+
+
+        $id = $this->session->userdata($data_session['usr_name']);
+        $data = $this->db->get_where('tik.mhs_jdwal',$where)->result_array();
+        return $data;
+    }
+
+    function LihatJadwalMahasiswa_get(){
+        // select * from tik.jadwal_kul where kodejdwl in (select jadwal_kul_kodejdwl from tik.mhs_jdwal where mahasiswa_nim = '4617010043')
+
+
+        $id = $this->session->userdata($data_session['usr_name']);
+        #Create where clause
+        $this->db->select('select jadwal_kul_kodejdwl');
+        $this->db->from('tik.mhs_jdwal');
+        $this->db->where('mahasiswa_nim',$id);
+        $where_clause = $this->db->get_compiled_select();
+
+        #Create main query
+        $this->db->select('*');
+        $this->db->from('tik.mhs_jdwal');
+        $data = $this->db->get_where("`kodejdwl` IN ($where_clause)")->result_array();
+        return $data;
+
+
+    }
+}
+?>
---
+++ application/controllers/Login.php
@@ -0,0 +1,44 @@
+<?php
+
+defined ('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Login extends REST_Controller{
+
+	function __construct($config = 'rest'){
+		parent::__construct($config);
+	}
+
+	function login_post(){
+        $username = $this->post('username');
+        $password = $this->post('password');
+        $where = array(
+            'usr_name' => $username,
+            'password' => $password
+        )
+
+        $cek_m = $this->db->get_where('tik.mahasiswa',$where)->num_rows();
+        $cek_d = $this->db->get_where('tik.staff',$where)->num_rows();
+
+        if($cek_m > 0){
+            $data_session = array(
+                'usr_name' => $username,
+                'password' => $password
+            );
+            $this->session->set_userdata($data_session);
+        }
+        elseif ($cek_d > 0) {
+            $data_session = array(
+                'usr_name' => $username,
+                'password' => $password
+            );
+            $this->session->set_userdata($data_session);
+        }
+        else{
+            echo "gagal login";
+        }
+    }
+}
+?>
