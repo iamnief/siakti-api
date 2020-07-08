3d2ffd86be1fc02869aac84e30304f796615f73a
Ismail Adi <ismailadiazz@gmail.com>
2020-07-08 14:15:06 +0700

    update: mahasiswa put method

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -73,39 +73,13 @@ class Mahasiswa extends REST_Controller{
 
     function index_put(){
         $nim = $this->put('nim');
-
-        $data = array(
-            'nim' => $nim,
-            'nama_mhs' => $this->put('nama_mhs'),
-            'add_mhs' => $this->put('add_mhs'),
-            'add_kel_mhs' => $this->put('add_kel_mhs'),
-            'add_kec_mhs' => $this->put('add_kec_mhs'),
-            'add_kota_mhs' => $this->put('add_kota_mhs'),
-            'bpk_mhs' => $this->put('bpk_mhs'),
-            'add_bpk_mhs' => $this->put('add_bpk_mhs'),
-            'kel_bpk' => $this->put('kel_bpk'),
-            'kec_bpk' => $this->put('kec_bpk'),
-            'kota_bpk' => $this->put('kota_bpk'),
-            'ibu_mhs' => $this->put('ibu_mhs'),
-            'add_ibu_mhs' => $this->put('add_ibu_mhs'),
-            'kel_ibu' => $this->put('kel_ibu'),
-            'kec_ibu' => $this->put('kec_ibu'),
-            'kota_ibu' => $this->put('kota_ibu'),
-            'tlp_mhs' => $this->put('tlp_mhs'),
-            'tlp_bpk' => $this->put('tlp_bpk'),
-            'tlp_ibu' => $this->put('tlp_ibu'),
-            'profesi_bpk' => $this->put('profesi_bpk'),
-            'profesi_ibu' => $this->put('profesi_ibu'),
-            'penghasilan_bpk' => $this->put('penghasilan_bpk'),
-            'email_mhs' => $this->put('email_mhs'),
-            'email_ortu' => $this->put('email_ortu'),
-            'kelas_kodeklas' => $this->put('kelas_kodeklas'),
-            'thn_akad_thn_akad_id' => $this->put('thn_akad_thn_akad_id'),
-            'usr_name' => $this->put('usr_name'),
-            'password' => $this->put('password'),
-            'pin' => $this->put('pin'),
-            'prodi_prodi_id' => $this->put('prodi_prodi_id')
-        );
+        if(!$nim){
+            $this->response([
+                'responseCode' => '01',
+                'responseDesc' => 'Failed to Update Data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        $data = $this->put();
 
         if($this->mahasiswa->update($data) > 0){
             $this->response([
